9e4ea8b281f9595516d71253dcb11fc117dd1d6d
Sunny Wadkar <sunny@Sunnys-MacBook-Pro.local>
2019-05-10 00:59:42 +0530

    Put encryption key in separate file

--- tea_impl.c
+++ tea_impl.c
@@ -1,7 +1,5 @@
 #include "tea_impl.h"
 
-unsigned32_t encryption_key[4]={30,40,50,60}; // 128-bit key
-
 void tea_encrypt (unsigned32_t* plaintext)
 {
     unsigned32_t plaintext_part1 = plaintext[0], plaintext_part2 = plaintext[1], key_schd_sum = 0, i;
