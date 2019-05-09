67549e77c0b01cfd63780c5b3b7531346250a8d2
Sunny Wadkar <sunny@Sunnys-MacBook-Pro.local>
2019-05-10 00:08:39 +0530

    Restructed code

--- tea_impl.c
+++ tea_impl.c
@@ -1,42 +1,43 @@
 #include "stdio.h"
 
-typedef long uint32_t;
+typedef long unsigned32_t;
 
-void tea_encrypt (uint32_t v[2], uint32_t k[4]);
-void tea_decrypt (uint32_t v[2], uint32_t k[4]);
+const unsigned32_t key_schd_const = 0x9E3779B9;  //key scheduling constant calculated as (2^32)/golden ratio 1.6180339887
 
-const uint32_t delta = 0x9E3779B9;  //key scheduling constant calculated as (2^32)/golden ratio 1.6180339887
+void tea_encrypt (unsigned32_t* plaintext, unsigned32_t* enc_key)
+{
+    unsigned32_t plaintext_part1 = plaintext[0], plaintext_part2 = plaintext[1], key_schd_sum = 0, i;
+    for (i=0; i<32; i++)  // 32 rounds of encoding
+    {
+        key_schd_sum += key_schd_const;
+        plaintext_part1 += ((plaintext_part2<<4) + *enc_key) ^ (plaintext_part2 + key_schd_sum) ^ ((plaintext_part2>>5) + *(enc_key+1));
+        plaintext_part2 += ((plaintext_part1<<4) + *(enc_key+2)) ^ (plaintext_part1 + key_schd_sum) ^ ((plaintext_part1>>5) + *(enc_key+3));
+    }
+    *plaintext = plaintext_part1;
+    *(plaintext+1) = plaintext_part2;
+}
+
+void tea_decrypt (unsigned32_t* crypttext, unsigned32_t* enc_key)
+{
+    unsigned32_t crypttext_part1 = crypttext[0], crypttext_part2 = crypttext[1], i;
+    unsigned32_t key_schd_sum = key_schd_const<<5;
+    for (i=0; i<32; i++)
+    {
+        crypttext_part2 -= ((crypttext_part1<<4) + *(enc_key + 2)) ^ (crypttext_part1 + key_schd_sum) ^ ((crypttext_part1>>5) + *(enc_key + 3));
+        crypttext_part1 -= ((crypttext_part2<<4) + *enc_key) ^ (crypttext_part2 + key_schd_sum) ^ ((crypttext_part2>>5) + *(enc_key+1));
+        key_schd_sum -= key_schd_const;
+    }
+    *crypttext = crypttext_part1;
+    *(crypttext + 1) = crypttext_part2;
+}
 
 int main()
 {
-  uint32_t num[2]={10,20};
-  uint32_t key[4]={30,40,50,60};
-  tea_encrypt(num,key);
-  printf("%ld,%ld \n",num[0],num[1]);
-  tea_decrypt(num,key);
-  printf("%ld,%ld \n",num[0],num[1]);
+  unsigned32_t num[2]={10,20};
+  unsigned32_t encryption_key[4]={30,40,50,60};
+  tea_encrypt(num,encryption_key);
+  printf("After Encryption: %ld,%ld \n",num[0],num[1]);
+  tea_decrypt(num,encryption_key);
+  printf("After Decryption: %ld,%ld \n",num[0],num[1]);
   return 0;
-}
-
-void tea_encrypt (uint32_t v[2], uint32_t k[4]) {
-    uint32_t v0=v[0], v1=v[1], sum=0, i;
-    uint32_t k0=k[0], k1=k[1], k2=k[2], k3=k[3];
-    for (i=0; i<32; i++) {
-        sum += delta;
-        v0 += ((v1<<4) + k0) ^ (v1 + sum) ^ ((v1>>5) + k1);
-        v1 += ((v0<<4) + k2) ^ (v0 + sum) ^ ((v0>>5) + k3);
-    }
-    v[0]=v0; v[1]=v1;
-}
-
-void tea_decrypt (uint32_t v[2], uint32_t k[4]) {
-    uint32_t v0=v[0], v1=v[1], sum, i;
-    uint32_t k0=k[0], k1=k[1], k2=k[2], k3=k[3];
-    sum = delta<<5;
-    for (i=0; i<32; i++) {
-        v1 -= ((v0<<4) + k2) ^ (v0 + sum) ^ ((v0>>5) + k3);
-        v0 -= ((v1<<4) + k0) ^ (v1 + sum) ^ ((v1>>5) + k1);
-        sum -= delta;
-    }
-    v[0]=v0; v[1]=v1;
 }
