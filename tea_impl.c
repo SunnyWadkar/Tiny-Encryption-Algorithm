8e4e72202195bdc3f35fb8715bd939153a8fe30d
Sunny Wadkar <sunny@Sunnys-MacBook-Pro.local>
2019-05-09 23:20:57 +0530

    Made delta as constant

--- tea_impl.c
+++ tea_impl.c
@@ -5,6 +5,8 @@ typedef long uint32_t;
 void encrypt (uint32_t v[2], uint32_t k[4]);
 void decrypt (uint32_t v[2], uint32_t k[4]);
 
+const uint32_t delta = 0x9E3779B9;
+
 int main()
 {
   uint32_t num[2]={10,20};
@@ -17,27 +19,24 @@ int main()
 }
 
 void encrypt (uint32_t v[2], uint32_t k[4]) {
-    uint32_t v0=v[0], v1=v[1], sum=0, i;           /* set up */
-    uint32_t delta=0x9E3779B9;                     /* a key schedule constant */
+    uint32_t v0=v[0], v1=v[1], sum=0, i;
     uint32_t k0=k[0], k1=k[1], k2=k[2], k3=k[3];   /* cache key */
-    for (i=0; i<32; i++) {                         /* basic cycle start */
+    for (i=0; i<32; i++) {
         sum += delta;
         v0 += ((v1<<4) + k0) ^ (v1 + sum) ^ ((v1>>5) + k1);
         v1 += ((v0<<4) + k2) ^ (v0 + sum) ^ ((v0>>5) + k3);
-    }                                              /* end cycle */
+    }
     v[0]=v0; v[1]=v1;
 }
 
 void decrypt (uint32_t v[2], uint32_t k[4]) {
-    uint32_t v0=v[0], v1=v[1], sum, i;  /* set up; sum is 32*delta */
-    uint32_t delta=0x9E3779B9;                     /* a key schedule constant */
-    uint32_t k0=k[0], k1=k[1], k2=k[2], k3=k[3];   /* cache key */
+    uint32_t v0=v[0], v1=v[1], sum, i;
+    uint32_t k0=k[0], k1=k[1], k2=k[2], k3=k[3];
     sum = delta<<5;
-    printf("%lx \n",sum);
-    for (i=0; i<32; i++) {                         /* basic cycle start */
+    for (i=0; i<32; i++) {
         v1 -= ((v0<<4) + k2) ^ (v0 + sum) ^ ((v0>>5) + k3);
         v0 -= ((v1<<4) + k0) ^ (v1 + sum) ^ ((v1>>5) + k1);
         sum -= delta;
-    }                                              /* end cycle */
+    }
     v[0]=v0; v[1]=v1;
 }
