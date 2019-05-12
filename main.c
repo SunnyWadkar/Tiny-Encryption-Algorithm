4cf5309f4544ed847e292acb4a71c480f9c52b72
Sunny Wadkar <sunny@Sunnys-MacBook-Pro.local>
2019-05-12 11:57:13 +0530

    Added a check for operation success/failure

--- main.c
+++ main.c
@@ -5,10 +5,18 @@
 int main(int argc, char *argv[])
 {
   unsigned32_t num[4]={10,20,30,40};
+  enc_return_type_t encOperationStat;
   printf("Before Encryption: %ld,%ld,%ld,%ld \n",num[0],num[1],num[2],num[3]);
-  teaOperateBlock(num,16,ENCRYPTION);
-  printf("After Encryption: %ld,%ld,%ld,%ld \n",num[0],num[1],num[2],num[3]);
-  teaOperateBlock(num,16,DECRYPTION);
-  printf("After Decryption: %ld,%ld,%ld,%ld \n",num[0],num[1],num[2],num[3]);
+  encOperationStat = teaOperateBlock(num,16,ENCRYPTION);
+  if(encOperationStat == OPERATION_SUCCESS)
+  {
+      printf("After Encryption: %ld,%ld,%ld,%ld \n",num[0],num[1],num[2],num[3]); //Second argument is number of bytes
+      teaOperateBlock(num,16,DECRYPTION);
+      printf("After Decryption: %ld,%ld,%ld,%ld \n",num[0],num[1],num[2],num[3]);
+  }
+  else
+  {
+      printf("Encryption Failure!! \n");
+  }
   return 0;
 }
